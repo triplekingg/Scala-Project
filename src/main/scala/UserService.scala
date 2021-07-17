6ea6d27e609c69ebfecafd14e13a44c0ad18c817
Manav <Tripleking123@gmail.com>
2021-07-18 02:57:50 +0700

    Implement Parser in Calculator and test(Fixed)

--- src/main/scala/UserService.scala
+++ src/main/scala/UserService.scala
@@ -1,6 +1,6 @@
 import java.util
-import java.util.HashMap
-import java.util.Scanner
+import java.util.{HashMap, InputMismatchException, Scanner}
+import scala.util.control.Breaks._
 
 class UserService {
   val calculator = new Calculator;
@@ -21,15 +21,22 @@ class UserService {
     println(s"Percentage of fund left: $left%")
     val used = calculator.percentageUsed(income,expense)
     println(s"Percentage of fund used: $used%")
-    println("Enter the important expenses in the format shown below, type DONE to proceed after entering the values")
+    println("Enter the important expenses in the format shown below, hit enter to proceed after entering the values")
     println("<Expense name> <Expense>")
+    var a = sc.nextLine()
     try{
-      while(sc.nextLine()!="DONE"){
-      calculator.parser(sc.nextLine(),importantExpenses)
-    }}
-    catch {
-      case e:NumberFormatException => println("You have no Important Expenses")
+      while (a != "DONE") {
+        a = sc.nextLine()
+        calculator.parser(a, importantExpenses);
+      }
+      if (a != "DONE") calculator.parser(sc.nextLine(), importantExpenses);
     }
+    catch {
+      case e:NumberFormatException => println("Skipped")
+      case e:InputMismatchException => println("Wrong Format")
+    }
+
+
 
     println("Enter the percentage that you want to cut down your expenses by");
     val percent = sc.nextDouble();
